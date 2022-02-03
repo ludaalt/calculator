2f64500f877d3fd5f62c3fe35283a1866d8c68d8
Luda <luda.tamb@mail.ru>
2022-02-03 15:22:49 +0300

    feat: implement basic input display

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,46 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const InputDisplay = styled.input`
-    width: 100%;
-    background: transparent;
+const Area = styled.div`
+    margin: 20px 0;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    border-radius: 30px;
+
+    height: 200px;
+    background: white;
+    font-size: 40px;
+    font-family: 'Roboto';
+    color: gray;
+    text-align: end;
+
+    div {
+        min-height: 20px;
+        outline: none;
+        overflow: hidden;
+
+        border: 3px solid red;
+    }
 
 `
 
-const Input = () => {
-    const [value, setValue] = useState(0);
+const Input = ({ currentValue }) => {
+    const [value, setValue] = useState(currentValue);
 
     const setValueHandler = (e) => {
-        setValue(e.target.value)
+        setValue(e.target.textContent)
+        console.log(value)
     }
-    return (
-        <InputDisplay placeholder="place" value={value} onChange={(e) => setValueHandler(e)} />
-    )
 
+    return (
+        <Area>
+            <div contentEditable value={value} onInput={setValueHandler}></div>
+        </Area>
+
+    )
 }
 
 export default Input
